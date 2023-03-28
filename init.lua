141bbbb817cfc2c40b074896b1b52e95757d07a5
Futuray-pgm <123812211+Futuray-pgm@users.noreply.github.com>
2023-03-28 12:50:33 +0200

    Test a possibility

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ activated_wand = {}
 powerless_wand.use = function(itemstack, user, pointed)
 	local pointed_node = minetest.get_node(pointed.under).name
 	if pointed_node == "magic_tech:infested_stone" then
-		minetest.place_node(pointed.under, "default:stone")
+		minetest.place_node(pointed.under, "default\:stone")
 	end
 	return 
 end
@@ -16,7 +16,7 @@ activated_wand.use = function(itemstack, user, pointed)
 	minetest.chat_send_all(pointed_node)
 	minetest.chat_send_all("default:stone")
 	if pointed_node == "default:stone" then
-		minetest.place_node(pointed.under, "magic_tech:infested_stone")
+		minetest.place_node(pointed.under, "magic_tech\:infested_stone")
 	end
 	return 
 end
