57db8ffe106ccbff4d3f2ed71547247f45f413d5
Futuray-pgm <123812211+Futuray-pgm@users.noreply.github.com>
2023-10-11 21:29:30 +0200

    Update init.lua to v0.0.3

--- init.lua
+++ init.lua
@@ -1,26 +1,25 @@
-local path = minetest.get_modpath("magic_tech")
-local storage = dofile(path .. "/machines.lua")
-
-print("magic_tech has loaded")
-
-powerless_wand = {}
-activated_wand = {}
-
--- Functions
-powerless_wand.use = function(itemstack, user, pointed)
-	local pointed_node = minetest.get_node(pointed.under).name
-	if pointed_node == "magic_tech:infested_stone" then
-		minetest.set_node(pointed.under, {name = "default:stone"})
+-- constants for all the scripts
+path = minetest.get_modpath("magic_tech")
+infastable_nodes = {"stone", "sand", "cobble", "dirt", "silver_sand"}
+version = "0.0.3"
+function node_in_group(name, group)
+	if not minetest.registered_nodes[name] or not minetest.registered_nodes[name].groups[group] then
+		return false
 	end
-	return 
+	return true
 end
-activated_wand.use = function(itemstack, user, pointed)
-	local pointed_node = minetest.get_node(pointed.under).name
-	if pointed_node == "default:stone" then
-		minetest.set_node(pointed.under, {name = "magic_tech:infested_stone"})
-	end
-	return 
-end
+
+minetest.register_on_joinplayer( function(player)
+	local name = player:get_player_name()
+	minetest.show_formspec(name, "magic_tech:welcome_form", "size[8,9]" ..
+				"label[0,0;" .. minetest.colorize("#808", "Hello, " .. name .. "! Thanks for downloading Magic Tech :)") .. "]" ..
+				"label[0,1.5;" .. minetest.colorize("#B7B", "Changes in version " .. version .. ":") .. "]" ..
+				"label[0,2.25;" .. minetest.colorize("#B7B", "  · new structure node") .. "]" ..
+				"label[0,3;" .. minetest.colorize("#B7B", "  · new water purifier") .. "]" ..
+				"label[0,3.75;" .. minetest.colorize("#B7B", "  · more infested nodes") .. "]" ..
+				"label[0,4.5;" .. minetest.colorize("#B7B", "  · this message") .. "]" ..
+				"button_exit[2,8;4,1;exit;OK, let's play!]")
+end)
 
 -- Crystal
 minetest.register_craftitem("magic_tech:crystal", {
@@ -35,51 +34,17 @@ minetest.register_node("magic_tech:stone_with_crystal", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
--- Wand
-minetest.register_tool("magic_tech:powerless_wand", {
-	description = "powerless wand",
-	inventory_image = "powerless_wand.png",
-	on_place = function(itemstack, user, pointed)
-		powerless_wand.use(itemstack, user, pointed)
-		return itemstack
-	end,
-})
-minetest.register_tool("magic_tech:activated_wand", {
-	description = "activated wand",
-	inventory_image = "activated_wand.png",
-	on_place = function(itemstack, user, pointed)
-		activated_wand.use(itemstack, user, pointed)
-		return
-	end,
-})
-
--- Other
-minetest.register_node("magic_tech:infested_stone", {
-	description = "infested stone",
-	tiles = {"default_stone.png^infested_node.png"},
-	groups = {cracky = 1},
-	drop = "magic_tech:infested_stone",
-	sounds = default.node_sound_stone_defaults(),
-})
-
--- Crafts
-minetest.register_craft({
-    type = "shaped",
-    output = "magic_tech:powerless_wand 1",
-    recipe = {
-        {"magic_tech:crystal"},
-        {"group:stick"}
-    }
-})
-minetest.register_craft({
-    type = "shaped",
-    output = "magic_tech:activated_wand 1",
-    recipe = {
-        {"", "default:mese_crystal", ""},
-        {"default:mese_crystal", "magic_tech:powerless_wand", "default:mese_crystal"},
-        {"", "default:mese_crystal", ""}
-    }
-})
+-- Infested nodes
+for i=1,table.getn(infastable_nodes) do
+	node = infastable_nodes[i]
+	minetest.register_node("magic_tech:infested_" .. node, {
+		description = "infested " .. node:gsub("_", " "),
+		tiles = {"default_" .. node ..".png^infested_node.png"},
+		groups = {cracky = 2, infested = 1},
+		drop = "magic_tech:infested_" .. node,
+		sounds = default.node_sound_stone_defaults(),
+	})
+end
 
 -- Ore parameters
 minetest.register_ore({
@@ -92,7 +57,6 @@ minetest.register_ore({
 	y_max          = 31000,
 	y_min          = 1025,
 })
-
 minetest.register_ore({
 	ore_type       = "scatter",
 	ore            = "magic_tech:stone_with_crystal",
@@ -103,7 +67,6 @@ minetest.register_ore({
 	y_max          = 64,
 	y_min          = -127,
 })
-
 minetest.register_ore({
 	ore_type       = "scatter",
 	ore            = "magic_tech:stone_with_crystal",
@@ -114,3 +77,7 @@ minetest.register_ore({
 	y_max          = -128,
 	y_min          = -31000,
 })
+
+-- import scripts
+dofile(path .. "/machines.lua")
+dofile(path .. "/scripts/wands.lua")
